d9fabe9b04edfc08577b6f8b8b0fa08ad95301c4
JorisDDaems <jorisddaems@gmail.com>
2021-01-18 12:36:10 +0100

    first commit recipe project

---
+++ src/main/java/be/intecbrussel/recipeproject/controllers/IndexController.java
@@ -0,0 +1,7 @@
+package be.intecbrussel.recipeproject.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+}
---
+++ src/test/java/be/intecbrussel/recipeproject/RecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package be.intecbrussel.recipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
