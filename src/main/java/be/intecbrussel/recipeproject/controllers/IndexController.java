02ca7019203cf174c22d0a2211aa0853f7fc0853
JorisDDaems <jorisddaems@gmail.com>
2021-01-21 10:41:17 +0100

    Created entities Notes and Recipe + setting jpa annotations

--- src/main/java/be/intecbrussel/recipeproject/controllers/IndexController.java
+++ src/main/java/be/intecbrussel/recipeproject/controllers/IndexController.java
@@ -1,7 +1,15 @@
 package be.intecbrussel.recipeproject.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+
+    @RequestMapping({"", "/","/index"})
+    public String getIndexPage(){
+        System.out.println("some message to say.. ");
+        return "index";
+    }
+
 }
