e982c904768b3c6c280315420b23d49536843003
JorisDDaems <jorisddaems@gmail.com>
2021-02-03 16:40:57 +0100

    load image from database implemented

--- src/main/java/be/intecbrussel/recipeproject/commands/RecipeCommand.java
+++ src/main/java/be/intecbrussel/recipeproject/commands/RecipeCommand.java
@@ -25,6 +25,7 @@ public class RecipeCommand {
     private Set<IngredientCommand> ingredients = new HashSet<>();
     private Difficulty difficulty;
     private NotesCommand notes;
+    private Byte[] image;
     private Set<CategoryCommand> categories = new HashSet<>();
 
 
