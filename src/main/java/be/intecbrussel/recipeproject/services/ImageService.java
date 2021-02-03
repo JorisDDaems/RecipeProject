4ead819ee4b458a9f2583e4cc1d5013b0570d166
JorisDDaems <jorisddaems@gmail.com>
2021-02-03 15:54:53 +0100

    Image uploading classes implemented

---
+++ src/main/java/be/intecbrussel/recipeproject/services/ImageService.java
@@ -0,0 +1,9 @@
+package be.intecbrussel.recipeproject.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+
+    void saveImageFile(Long recipeId, MultipartFile file);
+
+}
